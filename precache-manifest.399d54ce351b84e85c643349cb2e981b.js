7f8c224c42d8be737a601d1a5bfd547c1b9eae9a
Manish-Mulchandani <manishm9730@gmail.com>
2020-08-05 02:11:48 +0530

    Updates

---
+++ precache-manifest.399d54ce351b84e85c643349cb2e981b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a1fbff1bfa14e3548438b563b70b7a",
+    "url": "/Amazon-Clone-React/index.html"
+  },
+  {
+    "revision": "184a01df740168b049e6",
+    "url": "/Amazon-Clone-React/static/css/main.d33462c1.chunk.css"
+  },
+  {
+    "revision": "36baaa449c92bcb02c27",
+    "url": "/Amazon-Clone-React/static/js/2.77003dcc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Amazon-Clone-React/static/js/2.77003dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184a01df740168b049e6",
+    "url": "/Amazon-Clone-React/static/js/main.91ec4876.chunk.js"
+  },
+  {
+    "revision": "cc0621cc31a15641a0e9",
+    "url": "/Amazon-Clone-React/static/js/runtime-main.91200fc0.js"
+  }
+]);
